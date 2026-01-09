b11af922a87f5f6fe3360de0c632267d65b4a2f2
PauloS5 <paulosocratesdesouza@gmail.com>
2026-01-09 12:02:30 -0500

    feat: criar o arquivo principal das ferramentas iniciais do projeto

---
+++ apps/calc/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Calc" << endl;
+
+    return 0;
+}
---
+++ apps/matrix/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Matrix" << endl;
+
+    return 0;
+}
