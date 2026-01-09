84ffb855c607ae2800b1d409d58cb1f3a7d6de79
PauloS5 <paulosocratesdesouza@gmail.com>
2026-01-09 12:07:39 -0500

    feat: implementa as operações básicas de adição, subtração, multiplicação e divisão no sistema

---
+++ apps/calc/operations/basic.cpp
@@ -0,0 +1,12 @@
+double sum(double x, double y) {
+    return x+y;
+}
+double sub(double x, double y) {
+    return x-y;
+}
+double mult(double x, double y) {
+    return x*y;
+}
+double div(double x, double y) {
+    return x/y;
+}
